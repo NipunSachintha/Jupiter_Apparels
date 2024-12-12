302cf667d6361e178ad9f6731ae1519518903cd9
Nipun Sachintha <157737507+NipunSachintha@users.noreply.github.com>
2024-12-12 22:13:17 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom'
+import Home from './pages/Home'
+
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home/>} />
+        </Routes>
+        </BrowserRouter>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
