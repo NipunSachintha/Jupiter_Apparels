6715625c55098e3892f412479e33044ffad643ef
NS <157737507+NipunSachintha@users.noreply.github.com>
2025-01-14 14:38:44 +0530

    alert slice added

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,10 +1,12 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/userSlice';
+import alertReducer from './features/alertSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    alerts: alertReducer,
   }, 
 });
 
